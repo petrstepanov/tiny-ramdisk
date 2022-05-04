e56eacfff9b13b9d3fbcf45e0be6cd0289b35ceb
Petr Stepanov <stepanovps@gmail.com>
2022-05-04 01:06:01 -0400

    Lol

--- install2.sh
+++ install2.sh
@@ -7,11 +7,13 @@ cp ./src2/tiny-ramdisk-ramfs-startup.sh ./src2/tiny-ramdisk-ramfs-startup-$USER.
 sed -i "s;USER;$USER;g" ./src2/tiny-ramdisk-ramfs-startup-$USER.sh
 sed -i "s;HOME;$HOME;g" ./src2/tiny-ramdisk-ramfs-startup-$USER.sh
 sudo cp ./src2/tiny-ramdisk-ramfs-startup-$USER.sh /usr/bin
+#sudo chown root:root /usr/bin/tiny-ramdisk-ramfs-startup-$USER.sh
 
 cp ./src2/tiny-ramdisk-ramfs-shutdown.sh ./src2/tiny-ramdisk-ramfs-shutdown-$USER.sh 
 sed -i "s;USER;$USER;g" ./src2/tiny-ramdisk-ramfs-shutdown-$USER.sh
 sed -i "s;HOME;$HOME;g" ./src2/tiny-ramdisk-ramfs-shutdown-$USER.sh
 sudo cp ./src2/tiny-ramdisk-ramfs-shutdown-$USER.sh /usr/bin
+#sudo chown root:root /usr/bin/tiny-ramdisk-ramfs-startup-$USER.sh
 
 sudo chmod +x /usr/bin/tiny-ramdisk-ramfs*
 sudo chown root:root /usr/bin/tiny-ramdisk-ramfs*
@@ -22,6 +24,7 @@ sudo chown root:root /usr/bin/tiny-ramdisk-ramfs*
 cp ./src2/tiny-ramdisk-ramfs.service ./src2/tiny-ramdisk-ramfs-$USER.service
 sed -i "s;USER;$USER;g" ./src2/tiny-ramdisk-ramfs-$USER.service
 sudo cp ./src2/tiny-ramdisk-ramfs-$USER.service /etc/systemd/system/
+sudo chown root:root /etc/systemd/system/tiny-ramdisk-ramfs-$USER.service
 
 # Enable systemd service
 systemctl daemon-reload
