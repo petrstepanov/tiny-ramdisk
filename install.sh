6a0e6e19b6ea2e1075c843e74c5216fe35ce7fe5
Petr Stepanov <stepanovps@gmail.com>
2022-05-03 20:54:41 -0400

    Lol

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
 #/bin/bash
 
-# Copy startup and shutdown scripts
-mkdir -p ${HOME}/.local/bin
-cp ./src/*.sh ${HOME}/.local/bin
-chmod +x ${HOME}/.local/bin/tiny-ramdisk*
+# If no argument supplied - install ramfs (dynamic ramdisk)
+if [ -z "$1" ]
+then
+	echo "No argument supplied - installing ramfs mount"
 
+	# Copy startup and shutdown scripts
+	sudo cp ./src/tiny-ramdisk-ramfs*.sh /usr/bin
+	sudo chmod +x /usr/bin/tiny-ramdisk-ramfs*
 
-# Copy systemd services
-# https://wiki.archlinux.org/title/systemd/User#Writing_user_units
-cp ./src/*.service ${HOME}/.config/systemd/user/
+	# Copy systemd services
+	# https://wiki.archlinux.org/title/systemd/User#Writing_user_units
+	sudo cp ./src/tiny-ramdisk-ramfs.service /etc/systemd/system/
 
-# Enable systemd service
-systemctl --user enable tiny-ramdisk
-systemctl --user start tiny-ramdisk
+	# Enable systemd service
+	# systemctl enable tiny-ramdisk-ramfs
+	# systemctl start tiny-ramdisk-ramfs
+fi
