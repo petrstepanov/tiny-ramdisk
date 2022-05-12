671ad08f07b8b61a1a81e75f813795b8edcf0d4c
Petr Stepanov <stepanovps@gmail.com>
2022-05-11 20:56:26 -0400

    Fix

--- src/tiny-ramdisk-ramfs-startup.sh
+++ src/tiny-ramdisk-ramfs-startup.sh
@@ -4,18 +4,23 @@
 mkdir -p HOME/RAMDisk
 
 # Mount ramfs to the RAMDisk folder
-mount -t ramfs -o size=20m tmpfs HOME/RAMDisk
+# mount -t ramfs -o size=20m ramfs HOME/RAMDisk
+mount -t ramfs none HOME/RAMDisk
 
 # Only for ramfs: chown the RAMDisk folder (by default its owned by root for security purposes)
 # This is why we cannot install the ramfs systemd service for per-user
 # tmpfs may be installed as per-user possibly
 # https://unix.stackexchange.com/questions/345937/mount-ramfs-for-all-users
 chown USER:USER HOME/RAMDisk
+chmod 755 HOME/RAMDisk
 
 # Copy persistent content to RAMDisk (if persistent folder exists)
 if [ -d "HOME/.RAMDisk" ] 
 then
     echo "Persistent folder found. Copying content..." 
     # rsync interprets a directory with no trailing slash as `copy this directory`, and a directory with a trailing slash as copy the contents of this directory
-    rsync -avzh HOME/.RAMDisk/ HOME/RAMDisk
+    # rsync -avzh HOME/.RAMDisk/ HOME/RAMDisk
+    
+    # copy preserving the ownership, mode and timestamps
+    sudo -u USER cp -p -r HOME/.RAMDisk/* HOME/RAMDisk/
 fi
