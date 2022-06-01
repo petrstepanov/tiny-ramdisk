b447d455834b1ed0c8b3b38e46c8f2fd7f56c3fd
Petr Stepanov <stepanovps@gmail.com>
2022-05-31 21:47:31 -0400

    Update sync script

--- src/tiny-ramdisk-ramfs-shutdown.sh
+++ src/tiny-ramdisk-ramfs-shutdown.sh
@@ -7,7 +7,15 @@ chown -R USER:USER HOME/.RAMDisk
 
 # Copy RAMDisk from memory to persistent folder
 # rsync interprets a directory with no trailing slash as `copy this directory`, and a directory with a trailing slash as copy the contents of this directory
-rsync --recursive --ignore-existing --delete HOME/RAMDisk/ HOME/.RAMDisk
+# --recursive           -r  recurse into directories
+# --links               -l  copy symlinks as symlinks
+# --ignore-existing         skip updating files that exist on receiver
+# --times               -t  preserve modification times
+# --verbose             -v  increase verbosity
+# --delete                  delete extraneous files from dest dirs
+
+
+rsync --recursive --links --ignore-existing --delete HOME/RAMDisk/ HOME/.RAMDisk
 
 # Unmount the volume
 umount HOME/RAMDisk
